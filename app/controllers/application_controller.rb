550528717ebc9b8364e0c8da4a7b28013641daa8
Łukasz Kamiński <kaminski91@gmail.com>
2015-06-06 18:35:08 +0200

    Set lang cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_api
+  before_action :load_lang
 
   require 'rest-client'
 
@@ -10,9 +11,14 @@ class ApplicationController < ActionController::Base
   	@api = RestClient::Resource.new( 'https://pvpc-core.herokuapp.com/api/v1', 'pvpc', 'pefalpe987' )
   end
 
+  def load_lang
+    I18n.locale = cookies[:pvpc_user_lang].try(:to_sym) || I18n.default_locale
+  end
+
   def set_lang
   	session[:return_to] ||= request.referer || root_url
   	I18n.locale = params[:lang].to_sym
+    cookies[:pvpc_user_lang] = params[:lang]
   	redirect_to session.delete(:return_to)
   end
 end
